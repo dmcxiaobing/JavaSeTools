a86c63a08ca1a6d4746e40130869830bb2031319
qq986945193 <986945193@qq.com>
2017-04-20 10:20:30 +0800

    commit enum

---
+++ src/com/qq986945193/javasetools/knowledge/EnumIntroduce.java
@@ -0,0 +1,22 @@
+package com.qq986945193.javasetools.knowledge;
+
+/* @Author ：程序员小冰
+*/
+/**
+ * enum枚举类的简单介绍。
+ */
+public class EnumIntroduce {
+
+	public static void main(String[] args) {
+		System.out.println(GradleEnum.GET);
+		System.out.println(GradleEnum.POST);
+	}
+
+}
+
+/**
+ * 这是一个枚举类
+ */
+enum GradleEnum {
+	GET, POST,
+}
