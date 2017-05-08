5f64a1c7b3eed4da154876edd582dfbe97acd218
qq986945193 <986945193@qq.com>
2017-05-08 10:39:41 +0800

    commit uuid

--- src/com/qq986945193/javasetools/utils/GetUUIDRandomUtils.java
+++ src/com/qq986945193/javasetools/utils/GetUUIDRandomUtils.java
@@ -1,6 +1,10 @@
 package com.qq986945193.javasetools.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
+
+import org.junit.Test;
 
 /**
  * @Author ：程序员小冰
@@ -21,4 +25,21 @@ public class GetUUIDRandomUtils {
 	public static String getUUIDRandomNum() {
 		return UUID.randomUUID().toString().replace("-", "").toUpperCase();
 	}
+
+	/**
+	 * 生成随机数唯一码：UUID 20位 根据时间生成
+	 */
+	public static String getRandomUUID() {
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");// 设置日期格式
+		String str = "";
+		for (int i = 0; i < 6; i++) {
+			str += String.valueOf((int) (Math.random() * 9));
+		}
+		// new Date()为获取当前系统时间
+		return df.format(new Date()) + str;
+	}
+	/*
+	 * @Test public void test(){ System.out.println(getUUIDRandomNum());
+	 * System.out.println(getRandomUUID()); }
+	 */
 }
