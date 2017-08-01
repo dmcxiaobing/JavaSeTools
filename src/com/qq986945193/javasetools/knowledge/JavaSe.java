2a945ff44d4603c0b0702ff9feced36e18df1f73
qq986945193 <986945193@qq.com>
2017-08-01 09:16:53 +0800

    commit

--- src/com/qq986945193/javasetools/knowledge/JavaSe.java
+++ src/com/qq986945193/javasetools/knowledge/JavaSe.java
@@ -1,9 +1,15 @@
 package com.qq986945193.javasetools.knowledge;
 
+import org.junit.Test;
+
 public class JavaSe {
 
 	public static void main(String[] args) {
-		
+		System.out.println(34/10);
+		System.out.println(30%10);
+		String urlString = "method=findByPage&pc=9";
+		int index = urlString.lastIndexOf("&pc=");
+		System.out.println(index);
+		System.out.println(urlString.substring(0, index));
 	}
-
 }
